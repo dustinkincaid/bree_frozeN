f07d6e1a7c1cc3be5855edd309413f669b67bece
dustinkincaid <Dustin.Kincaid@uvm.edu>
2020-09-15 18:41:49 -0400

    Added P data for 2014 and 2015 to the compiled dataset

--- 02_analysis scripts/compile_data.R
+++ 02_analysis scripts/compile_data.R
@@ -9,6 +9,25 @@
   {
   # Read in 2014 grab sample chemistry data for Missisquoi Bay
     mb_2014.raw <- read.csv("01_raw data/Copy of Lake Grab 2014_2-6-15_sb.csv", header=T, stringsAsFactors = F, na.strings = c("", " ", "#N/A"))
+    
+  # Read in 2014 SRP Miss Bay data from grab samples
+    # SRP are ug P/L
+    mb_SRP <- read.csv("01_raw data/SRP Data 2014.csv", header = T, stringsAsFactors = F) %>% 
+      mutate(date = mdy(date)) %>% 
+      mutate(site = replace(site, site == "S087", "mb")) %>% 
+      mutate(samp_depth_cat = ifelse(depth %in% c("Below ice", "Surface"), "D1",
+                                     ifelse(depth == "1m", "D2",
+                                            ifelse(depth == "B+1m", "D3",
+                                                   ifelse(depth == "B+0.5m", "D4", "D5"))))) %>% 
+      mutate(samp_depth_cat2 = ifelse(depth %in% c("Below ice", "Surface"), "Top",
+                                     ifelse(depth == "1m", "Mid-1",
+                                            ifelse(depth == "B+1m", "Mid-2",
+                                                   ifelse(depth == "B+0.5m", "Mid-3", "Bottom"))))) %>%   
+      select(-depth) %>% 
+      mutate(rep = 1) %>% 
+      rename(depth = depth_m, SRP = PO4_ugL) %>% 
+      # Convert SRP to mg/l from ug/L to match other grab sample data
+      mutate(SRP = SRP/1000)
   
   # Read in 2014 manual sonde cast data for Missisquoi Bay
     mb_sensor_2014 <- read.csv("01_raw data/2014_Manual_Sonde_Casts_Winter.csv", header=T, stringsAsFactors = F)
@@ -20,36 +39,68 @@
     sp_n.raw <- read.csv("01_raw data/WINTER 2015 SP_TN-TP.csv", header=T, stringsAsFactors = F)
   
   # Read in 2015 supplementary data from Joung et al. 2017
-  # Keep the sensor data
+  # Keep the sensor
     mb_sensor <- read.csv("01_raw data/WINTER 2015 MB_supp_data.csv", header=T, stringsAsFactors = F, na.strings = " ") %>% 
       filter(Location == "Lake") %>% 
       select(Cal.Date, Depth_sensor:BGA) %>% 
       rename(date=Cal.Date, depth=Depth_sensor, temp=Temp., cond=Cond., turb=Turb., chla=Chl.a) %>% 
-      mutate(date = mdy(as.character(date), tz="US/Eastern"),
-             site = "mb")
+      mutate(date = mdy(as.character(date)),
+             site = "mb") %>% 
+      mutate(rep = 1)
     sp_sensor <- read.csv("01_raw data/WINTER 2015 SP_supp_data.csv", header=T, stringsAsFactors = F, na.strings = " ") %>% 
       select(Cal.Date, Depth_sensor:BGA) %>% 
       rename(date=Cal.Date, depth=Depth_sensor, temp=Temp., cond=Cond., turb=Turb., chla=Chl.a) %>% 
-      mutate(date = mdy(as.character(date), tz="US/Eastern"),
-             site = "sp")
-    
+      mutate(date = mdy(as.character(date)),
+             site = "sp") %>% 
+      mutate(rep = 1)
+  # Keep the 2015 P data from supp info file
+  # All P concentrations are umol/kg, which is umol/L (assuming 1 kg = 1 L for freshwater)
+    mb_P_15 <- read.csv("01_raw data/WINTER 2015 MB_supp_data.csv", header=T, stringsAsFactors = F, na.strings = " ") %>% 
+      filter(Location == "Lake") %>% 
+      select(date = Cal.Date, depth = Depth_grab, SRP, DOP, PP, TDP, TP) %>% 
+      filter(!is.na(SRP)) %>% 
+      mutate(date = mdy(as.character(date)),
+             site = "mb") %>% 
+      # For joining purposes below, let's convert umol/L to mg/L
+      mutate(SRP = SRP/1000*30.974,
+             DOP = DOP/1000*30.974,
+             PP = PP/1000*30.974,
+             TDP = TDP/1000*30.974,
+             TP = TP/1000*30.974) %>% 
+      mutate(rep = 1)
+    sp_P_15 <- read.csv("01_raw data/WINTER 2015 SP_supp_data.csv", header=T, stringsAsFactors = F, na.strings = " ") %>%
+      select(date = Cal.Date, depth = Depth_grab, SRP, DOP, PP, TDP, TP) %>% 
+      filter(!is.na(SRP)) %>% 
+      mutate(date = mdy(as.character(date)),
+             site = "sp") %>% 
+      # For joining purposes below, let's convert umol/L to mg/L
+      mutate(SRP = SRP/1000*30.974,
+             DOP = DOP/1000*30.974,
+             PP = PP/1000*30.974,
+             TDP = TDP/1000*30.974,
+             TP = TP/1000*30.974) %>% 
+      mutate(rep = 1)
+       
   # Read in ice depth data compiled from Schroth et al. 2015 and Joung et al. 2017
     ice <- read.csv("01_raw data/iceDepths.csv", header = T, stringsAsFactors = F, na.strings = "NA") %>% 
-      mutate(date = mdy(as.character(date), tz="US/Eastern"))
+      mutate(date = mdy(as.character(date)))
   }
 
 # Tidy the data----
   {
-  # Tidy the 2014 data
+  # Tidy the 2014 grab sample data 
     mb_2014 <- mb_2014.raw %>% 
       filter(Lab.ID.Bottle.ID != "LG14-013") %>% 
       select(Depth.or.BLANK, depth_cat, Date.Collected, TP.Corrected, Nox.Corrected, NH4.Corrected, TN.Corrected) %>% 
       filter(Depth.or.BLANK != "blank", Depth.or.BLANK != "blank ", Depth.or.BLANK != "Blank", !is.na(Depth.or.BLANK)) %>% 
       rename(date=Date.Collected, samp_depth_cat=depth_cat, samp_depth_desc=Depth.or.BLANK, TP=TP.Corrected, NO3=Nox.Corrected, NH4=NH4.Corrected, TN=TN.Corrected) %>% 
-      mutate(date = mdy(as.character(date), tz="US/Eastern"),
+      mutate(date = mdy(as.character(date)),
              site = "mb") %>% 
       # Filter for dates on or before 4/25/2014; last on ice sample = 4/3/2014
-      filter(date <= "2014-04-25 EDT")
+      filter(date <= "2014-04-25") %>% 
+      # Add rep no.
+      group_by(date, samp_depth_cat) %>% 
+      mutate(rep = row_number())
     
     # Add depths
       # NOTE: the bottom 3 depths are referenced to the bottom depth; DK estimated these bottom depths from manual sonde cast
@@ -60,7 +111,7 @@
                         0.5, 1, 2.3, 2.8, 3.3, #2014-03-05
                         0.5, 1, 2.1, 2.6, 3.1, #2014-03-19
                         0.5, 1, 2.1, 2.6, 3.1, #2014-04-03
-                        0.5, NA, 3.1, NA, 4.1  #2014-04-25
+                        0.5, 1, 3.1, 3.6, 4.1  #2014-04-25
                         )    
       # Create a df with values to match and merge with mb_2014
         mb2014_depths_df <- data.frame(date = rep(unique(mb_2014$date), each=5), samp_depth_cat = rep(unique(mb_2014$samp_depth_cat), 7), depth = mb2014_depths)
@@ -72,11 +123,14 @@
       depth_cat2 <- c("Top", "Mid-1", "Mid-2", "Mid-3", "Bottom")
       mb_2014$samp_depth_cat2 <- depth_cat2[match(mb_2014$samp_depth_cat, depth_index_2014)]
     
-    # Update Julian day
+    # Add 2014 SRP data to mb_2014 & update day of year
       mb_2014 <- mb_2014 %>% 
+        full_join(mb_SRP, by = c("date", "site", "depth", "rep")) %>% 
+        mutate(samp_depth_cat.x = ifelse(is.na(samp_depth_cat.x), samp_depth_cat.y, samp_depth_cat.x),
+               samp_depth_cat2.x = ifelse(is.na(samp_depth_cat2.x), samp_depth_cat2.y, samp_depth_cat2.x)) %>% 
         mutate(yday = yday(date)) %>% 
         # Re-order columns
-        select(date, yday, site, depth, everything()) %>% select(-samp_depth_desc)
+        select(date, yday, site, depth, samp_depth_cat = samp_depth_cat.x, samp_depth_cat2 = samp_depth_cat2.x, everything()) %>% select(-c(samp_depth_desc, samp_depth_cat.y, samp_depth_cat2.y))
     
   # Tidy the 2014 manual sonde cast data for Missisquoi Bay
       # I've made the assumption that cond in 2015 sensor data is cond, not specfic cond  
@@ -115,20 +169,20 @@
                    pH = round(pH, digits = 2),
                    turb = round(turb, digits = 1),
                    chla = round(chla, digits = 1),
-                   BGA = round(BGA, digits = 0))
+                   BGA = round(BGA, digits = 0)) %>% 
+            mutate(rep = 1) %>% 
+            mutate(site = "mb")
           
-    # Join the 2014 nutrient and sensor data
-        mb_2014 <- full_join(mb_2014, mb_sensor_2014, by = c("date", "yday", "depth")) %>%
-          # Sort by date a depth
-          arrange(date, depth) %>% 
-          # Add a site column
-          mutate(site = "mb")
+  # Join mb_2014 & mb_sensor_2014
+    mb_2014 <- full_join(mb_2014, mb_sensor_2014, by = c("date", "yday", "site", "depth", "rep")) %>% 
+      select(-rep) %>% 
+      arrange(date, depth)
   
   # Tidy the 2015 data and combine into one
     # Missisquoi Bay  
       mb_n <- mb_n.raw %>% 
-        select(Date.Collected, Depth.or.BLANK, TP.Corrected, Nox.Corrected, NH4.Corrected, TN.Corrected) %>% 
-        rename(date=Date.Collected, samp_depth_cat=Depth.or.BLANK, TP=TP.Corrected, NO3=Nox.Corrected, NH4=NH4.Corrected, TN=TN.Corrected) %>% 
+        select(Date.Collected, Depth.or.BLANK, Nox.Corrected, NH4.Corrected, TN.Corrected) %>% 
+        rename(date=Date.Collected, samp_depth_cat=Depth.or.BLANK, NO3=Nox.Corrected, NH4=NH4.Corrected, TN=TN.Corrected) %>% 
         mutate(date = mdy(as.character(date), tz="US/Eastern"),
                yday = yday(date),
                site = "mb")
@@ -148,15 +202,20 @@
                       )
       # Create a df with values to match and merge with mb_n
         mb_depths_df <- data.frame(date = rep(unique(mb_n$date), each=5), samp_depth_cat = rep(unique(mb_n$samp_depth_cat), 8), depth = mb_depths)
-      # Join to add depths to mb_n
-        mb_n <- full_join(mb_n, mb_depths_df, by = c("date", "samp_depth_cat"))
+      # Add depths to mb_n & join 2015 P data
+        mb_n <- full_join(mb_n, mb_depths_df, by = c("date", "samp_depth_cat")) %>% 
+          # Add rep no.
+          group_by(date, samp_depth_cat) %>% 
+          mutate(rep = row_number()) %>% 
+          # Join 2015 MP P data
+          full_join(mb_P_15, by = c("date", "site", "rep", "depth"))
       # Add sensor data from supplementary material
-        mb_n <- full_join(mb_n, mb_sensor, by = c("date", "site", "depth")) %>% arrange(date, depth)
+        mb_n <- full_join(mb_n, mb_sensor, by = c("date", "site", "rep", "depth")) %>% arrange(date, depth) %>% select(-rep)
   
     # Repeat for Shelburne Pond data
       sp_n <- sp_n.raw %>% 
-        select(Date.Collected, Depth.or.BLANK, TP.Corrected, Nox.Corrected, NH4.Corrected, TN.Corrected) %>% 
-        rename(date=Date.Collected, samp_depth_cat=Depth.or.BLANK, TP=TP.Corrected, NO3=Nox.Corrected, NH4=NH4.Corrected, TN=TN.Corrected) %>% 
+        select(Date.Collected, Depth.or.BLANK, Nox.Corrected, NH4.Corrected, TN.Corrected) %>% 
+        rename(date=Date.Collected, samp_depth_cat=Depth.or.BLANK, NO3=Nox.Corrected, NH4=NH4.Corrected, TN=TN.Corrected) %>% 
         mutate(date = mdy(as.character(date), tz="US/Eastern"),
                site = "sp")
       # Correct sampling dates
@@ -177,10 +236,15 @@
   
       # Create a df with values to match and merge with mb_n
         sp_depths_df <- data.frame(date = rep(unique(sp_n$date), each=5), samp_depth_cat = rep(unique(sp_n$samp_depth_cat), 8), depth = sp_depths)
-      # Join to add depths to mb_n
-        sp_n <- full_join(sp_n, sp_depths_df, by = c("date", "samp_depth_cat"))
+      # Add depths to sp_n & join 2015 P data
+        sp_n <- full_join(sp_n, sp_depths_df, by = c("date", "samp_depth_cat")) %>% 
+          # Add rep no.
+          group_by(date, samp_depth_cat) %>% 
+          mutate(rep = row_number()) %>% 
+          # Join 2015 SP P data
+          full_join(sp_P_15, by = c("date", "site", "rep", "depth"))        
       # Add sensor data from supplementary material
-        sp_n <- full_join(sp_n, sp_sensor, by = c("date", "site", "depth")) %>% arrange(date, depth)
+        sp_n <- full_join(sp_n, sp_sensor, by = c("date", "site", "rep", "depth")) %>% arrange(date, depth) %>% select(-rep)
       
       # Here's the final dataframe with 2015 data from both lakes
         winter2015_chem_all <- 
@@ -203,20 +267,22 @@
              NH4 = NH4*1000/14.007,
              TN = TN*1000/14.007,
              tntp = TN/TP) %>% 
-      select(site, date, yday, depth, samp_depth_cat, samp_depth_cat2, NH4, NO3, TN, TP, tntp, temp:BGA)
+      select(site, date, yday, depth, samp_depth_cat, samp_depth_cat2, NH4, NO3, TN, SRP, DOP, PP, TDP, TP, tntp, temp:BGA)
       # select(date:TN, tntp, temp:BGA, samp_depth_cat2) 
 
   # Remove unnecessary objects
     rm(depth_cat2, depth_index_2014, depth_index_2015, keys, mb_2014.raw, mb_depths, mb_depths_df, 
-       mb_sensor_2014, mb2014_depths_df, mb_n, mb_n.raw, sp_depths_df, sp_n, sp_n.raw, mb_sensor, sp_sensor)
+       mb_sensor_2014, mb2014_depths_df, mb_n, mb_n.raw, sp_depths_df, sp_n, sp_n.raw, mb_sensor, sp_sensor,
+       mb_P_15, sp_P_15, mb_SRP)
     
   # Add ice depth data
     alldata <- alldata %>% 
       full_join(ice, by = c("site", "date", "yday")) %>% 
       select(site, date, yday, ice_depth = ice_depth_m, everything())
-    
-  # Write these data to a CSV file
-    alldata %>%
-      mutate(date = as.character(date)) %>% 
-      write_csv("01_raw data/alldata_2014_2015_mb_sp_compiled.csv")
-  }
+  }   
+     
+
+# Write these data to a CSV file
+  alldata %>%
+    mutate(date = as.character(date)) %>% 
+    write_csv("01_raw data/alldata_2014_2015_mb_sp_compiled.csv")
