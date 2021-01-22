73e850185f9fd97e4a3ca0c6cf90370244de0769
dustinkincaid <Dustin.Kincaid@uvm.edu>
2021-01-22 10:47:19 -0500

    Updated SI figure to include conductivity, turbidity, all N species, and all P species

--- 02_analysis scripts/compare_lake_river_chem.R
+++ 02_analysis scripts/compare_lake_river_chem.R
@@ -1,25 +1,13 @@
+# Compare conductivity and turibity levels and N and P concentrations in lake vs. in river during melt period
+
+# Load packages
 library("tidyverse")
 library("lubridate")
-library("cowplot")
+library("patchwork")
 
 # All lake data compiled using compile_data.R; concentrations are in umol/L (uM)
 alldata <- read_csv("01_raw data/alldata_2014_2015_mb_sp_compiled.csv")
   
-# Pull Miss Riv data from Joung et al 2017 Supp Info
-# mb_chem <- read.csv("01_raw data/WINTER 2015 MB_supp_data.csv", header=T, stringsAsFactors = F, na.strings = " ") %>% 
-#   filter(location == "River") %>% 
-#   # select(-c(ends_with("SD"), ends_with("02"), ends_with("45"), ends_with("coll")))
-#   select(-(ends_with("SD"))) %>% 
-#   select(Cal.Date, Jul.Day, location, `Temp.`:DOC) %>% 
-#   rename(date=Cal.Date, yday = Jul.Day, depth=Depth_grab, temp = `Temp.`, cond = `Cond.`, turb = `Turb.`, chla = `Chl.a`) %>% 
-#   mutate(date = mdy(as.character(date)),
-#          site = "mb") %>% 
-#   # Convert NO3-N from mg N/L to umol/L
-#   mutate(NO3 = NO3*1000/14.007)
-# 
-# # Append both
-# all_chem <-
-#   bind_rows(alldata, mb_chem)
 
 # Create summary of N concs (means and SE) for MB 2015 and 2015 River
 n_summ <-
@@ -33,33 +21,17 @@ n_summ <-
             conc_mean = mean(conc, na.rm = T),
             conc_SE = sd(conc)/sqrt(n))
 
-
-# Create plot theme
-theme1 <-
-  theme_classic() +
-  theme(legend.title = element_text(size = 8),
-        legend.text = element_text(size = 8),
-        legend.key.size = unit(0.15, "in"))
-
-# Create plot comparing lake NO3 concs to river NO3 concs
-no3 <- n_summ %>% 
-  filter(var == "NO3") %>% 
-  filter(yday >= 84) %>% 
-  # filter(yday >= 84 & yday <= 115) %>% 
-  mutate(samp_depth_cat2 = replace(samp_depth_cat2, location == "River", "River")) %>% 
-  mutate(samp_depth_cat2 = factor(samp_depth_cat2, 
-                                  levels=c("Top", "Mid-1", "Mid-2", "Mid-3", "Bottom", "River"))) %>% 
-  ggplot(aes(x = as.factor(yday), y = conc_mean, fill = samp_depth_cat2)) +
-  geom_bar(position = position_dodge(width = 0.9, preserve = "single"), stat = "identity") +
-  geom_errorbar(aes(ymin = conc_mean-conc_SE, ymax = conc_mean+conc_SE), 
-                position = position_dodge(width = 0.9, preserve = "single"),
-                width=.2) +
-  scale_fill_manual(name="Depth/Source",
-                    values=c("gray90", "gray75", "gray60", "gray45", "gray30", "dodgerblue3")) +
-  ylab(expression(paste("NO"["3"]^" -", " (",mu,"mol"," l"^"-1",")"))) + xlab("Day of year") +
-  theme1
-
-# ggsave("03_figures/plot_compare_lake_river_conc.png", width = 5, height = 3, units = "in", dpi = 150)
+# Create summary of P concs (means and SE) for MB 2015 and 2015 River
+p_summ <-
+  alldata %>% 
+  filter((site == "mb" & year(date) == 2015) | location == "River") %>% 
+  filter(!is.na(samp_depth_cat2) | location == "River") %>% 
+  select(date, yday, location, samp_depth_cat, samp_depth_cat2, SRP:TP) %>% 
+  pivot_longer(cols = SRP:TP, names_to = "var", values_to = "conc") %>% 
+  group_by(date, yday, location, samp_depth_cat, samp_depth_cat2, var) %>% 
+  summarize(n = sum(!is.na(conc)),
+            conc_mean = mean(conc, na.rm = T),
+            conc_SE = sd(conc)/sqrt(n))
 
 # Create summary of cond & turb levels (means and SE) for MB 2015 and 2015 River
 ysi_summ <-
@@ -73,58 +45,157 @@ ysi_summ <-
             conc_mean = mean(value, na.rm = T),
             conc_SE = sd(value, na.rm = T)/sqrt(n))
 
-# Create plot comparing lake cond levels to river cond levels
-cond <- ysi_summ %>% 
+
+# Create plot themes
+theme_bar <- 
+    theme_bw() +
+    theme(panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank(),
+          strip.background = element_rect(fill = "white"),
+          axis.text.x = element_text(size = 9, angle = 90, vjust = 0.5),
+          legend.text = element_text(size = 8),
+          legend.title = element_text(size = 10),
+          legend.key.size = unit(0.15, "in"))
+
+# New facet labels
+facet_labels <- c("84" = "DOY 84", "87" = "DOY 87", "96" = "DOY 96")
+
+# Create plots comparing lake cond, turb, N, and P levels to corresponding river levels
+cond_facet <- ysi_summ %>% 
   filter(var == "cond") %>% 
   filter(yday >= 84) %>% 
-  mutate(depth_range = factor(depth_range, levels = c("[0,1]", "(1,2]", "(2,3]", "(3,4]", "River"), labels = c("0-1 m", "1-2 m", "2-3 m", "3-4 m", "River"))) %>% 
+  mutate(depth_range = factor(depth_range, levels = c("[0,1]", "(1,2]", "(2,3]", "(3,4]", "River"), labels = c("0-1m", "1-2m", "2-3m", "3-4m", "River"))) %>% 
   mutate(depth_range = replace(depth_range, location == "River", "River")) %>% 
-  ggplot(aes(x = as.factor(yday), y = conc_mean, fill = depth_range)) +
-  geom_bar(position = position_dodge(width = 0.9, preserve = "single"), stat = "identity") +
+  ggplot(aes(x = depth_range, y = conc_mean, fill = depth_range)) +
+  facet_wrap(~yday, ncol = 3, labeller=labeller(yday = facet_labels)) +
+  geom_bar(stat = "identity") +
   geom_errorbar(aes(ymin = conc_mean-conc_SE, ymax = conc_mean+conc_SE), 
                 position = position_dodge(width = 0.9, preserve = "single"),
                 width=.2) +
   scale_fill_manual(name="Depth/Source",
-                    values=c("gray75", "gray60", "gray45", "gray30", "dodgerblue3")) +
-  ylab(expression(Cond.~(mu*S~cm^{-1}))) + xlab("Day of year") +
-  theme1
-
-# Create plot comparing lake turb levels to river turb levels
-turb <- ysi_summ %>% 
+                    values=c("gray75", "gray60", "gray45", "gray30", "dodgerblue3")) +  
+  ylab(expression(Cond.~(mu*S~cm^{-1}))) + xlab("Depth or source") +
+  theme_bar +
+  theme(legend.position = "none",
+        axis.text.x = element_blank(),
+        axis.title.x = element_blank())
+  
+turb_facet <- ysi_summ %>% 
   filter(var == "turb") %>% 
   filter(yday >= 84) %>% 
-  mutate(depth_range = factor(depth_range, levels = c("[0,1]", "(1,2]", "(2,3]", "(3,4]", "River"), labels = c("0-1 m", "1-2 m", "2-3 m", "3-4 m", "River"))) %>% 
+  mutate(depth_range = factor(depth_range, levels = c("[0,1]", "(1,2]", "(2,3]", "(3,4]", "River"), labels = c("0-1m", "1-2m", "2-3m", "3-4m", "River"))) %>% 
   mutate(depth_range = replace(depth_range, location == "River", "River")) %>% 
-  ggplot(aes(x = as.factor(yday), y = conc_mean, fill = depth_range)) +
-  geom_bar(position = position_dodge(width = 0.9, preserve = "single"), stat = "identity") +
+  ggplot(aes(x = depth_range, y = conc_mean, fill = depth_range)) +
+  facet_wrap(~yday, ncol = 3, labeller=labeller(yday = facet_labels)) +
+  geom_bar(stat = "identity") +
   geom_errorbar(aes(ymin = conc_mean-conc_SE, ymax = conc_mean+conc_SE), 
                 position = position_dodge(width = 0.9, preserve = "single"),
                 width=.2) +
   scale_fill_manual(name="Depth/Source",
-                    values=c("gray75", "gray60", "gray45", "gray30", "dodgerblue3")) +
-  ylab("Turb. (NTU)") + xlab("Day of year") +
-  theme1
+                    values=c("gray75", "gray60", "gray45", "gray30", "dodgerblue3")) +  
+  ylab("Turb. (NTU)") + xlab("Depth or source") +
+  theme_bar +
+  theme(legend.position = "none",
+        axis.title.x = element_blank())
 
-# Create plot comparing lake NO3 concs to river NO3 concs
-# Stacked bar graph
-no3 <- n_summ %>% 
-  filter(var == "NO3") %>% 
+n_stack <- n_summ %>% 
   filter(yday >= 84) %>% 
-  # filter(yday >= 84 & yday <= 115) %>% 
-  mutate(samp_depth_cat2 = replace(samp_depth_cat2, location == "River", "River")) %>% 
+  select(-conc_SE) %>% 
+  pivot_wider(names_from = var, values_from = conc_mean) %>% 
+  # Calculate unmeasured N concentration  
+  mutate(N_other = TN - (NO3 + NH4)) %>% 
+  # Wide to long format
+  pivot_longer(cols = c(NO3, NH4, N_other), names_to = "var", values_to = "conc") %>% 
   mutate(samp_depth_cat2 = factor(samp_depth_cat2, 
                                   levels=c("Top", "Mid-1", "Mid-2", "Mid-3", "Bottom", "River"))) %>% 
-  ggplot(aes(x = as.factor(yday), y = conc_mean, fill = samp_depth_cat2)) +
-  geom_bar(position = position_dodge(width = 0.9, preserve = "single"), stat = "identity") +
-  geom_errorbar(aes(ymin = conc_mean-conc_SE, ymax = conc_mean+conc_SE), 
-                position = position_dodge(width = 0.9, preserve = "single"),
-                width=.2) +
-  scale_fill_manual(name="Depth/Source",
-                    values=c("gray90", "gray75", "gray60", "gray45", "gray30", "dodgerblue3")) +
-  ylab(expression(paste("NO"["3"]^" -", " (",mu,"mol"," l"^"-1",")"))) + xlab("Day of year") +
-  theme1
+  # Plot
+  ggplot(aes(x = samp_depth_cat2, y = conc, fill = var)) +
+  facet_wrap(~yday, ncol = 3, labeller=labeller(yday = facet_labels)) +
+  geom_bar(position = "stack", stat = "identity") +
+  scale_fill_manual(name = "N species",
+                    breaks = c("N_other", "NH4", "NO3"),
+                    labels = c("DON + PN", "NH4", "NO3"),
+                    values = c("gray80", "#1b9e77", "#7570b3")) +
+  ylab(expression(paste("Conc.", " (",mu,"mol N"," l"^"-1",")"))) +
+  xlab("Depth or source") +
+  theme_bar +
+  theme(axis.text.x = element_blank(),
+        axis.title.x = element_blank())
 
-# Combine into one plot
-fig1 <- plot_grid(cond, turb, no3, align = "hv", ncol = 1, labels = "auto")
-save_plot("03_figures/suppInfo_melt_lake_river_comparison.png", fig1, dpi = 150,
-          units = "in", base_height = 7, base_width = 5)
+p_stack <- p_summ %>% 
+  filter(yday >= 84) %>% 
+  filter(var %in% c("PP", "DOP", "SRP")) %>% 
+  mutate(samp_depth_cat2 = factor(samp_depth_cat2, 
+                                  levels=c("Top", "Mid-1", "Mid-2", "Mid-3", "Bottom", "River"))) %>% 
+  # Plot
+  ggplot(aes(x = samp_depth_cat2, y = conc_mean, fill = var)) +
+  facet_wrap(~yday, ncol = 3, labeller=labeller(yday = facet_labels)) +
+  geom_bar(position = "stack", stat = "identity") +
+  scale_fill_manual(name = "P species",
+                    breaks = c("PP", "DOP", "SRP"),
+                    labels = c("PP", "DOP", "SRP"),
+                    values = c("gray80", "#1b9e77", "#7570b3")) +
+  ylab(expression(paste("Conc.", " (",mu,"mol P"," l"^"-1",")"))) +
+  xlab("Depth or source") +
+  theme_bar +
+  theme(axis.title.x = element_text(margin = margin(t = 5, r = 0, b = 0, l = 0)))
+
+# Combine the subplots into one plot using patchwork (amazing!)
+fig1 <- cond_facet + turb_facet + n_stack + p_stack + plot_layout(ncol = 1)
+ggsave("03_figures/suppInfo_melt_lake_river_comparison.png", plot = fig1, width = 6, height = 7, units = "in", dpi = 150)
+
+
+
+# Older plots
+# Create plot themes
+# theme1 <-
+#   theme_classic() +
+#   theme(legend.title = element_text(size = 8),
+#         legend.text = element_text(size = 8),
+#         legend.key.size = unit(0.15, "in"))
+
+# cond <- ysi_summ %>% 
+#   filter(var == "cond") %>% 
+#   filter(yday >= 84) %>% 
+#   mutate(depth_range = factor(depth_range, levels = c("[0,1]", "(1,2]", "(2,3]", "(3,4]", "River"), labels = c("0-1m", "1-2m", "2-3m", "3-4m", "River"))) %>% 
+#   mutate(depth_range = replace(depth_range, location == "River", "River")) %>% 
+#   ggplot(aes(x = as.factor(yday), y = conc_mean, fill = depth_range)) +
+#   geom_bar(position = position_dodge(width = 0.9, preserve = "single"), stat = "identity") +
+#   geom_errorbar(aes(ymin = conc_mean-conc_SE, ymax = conc_mean+conc_SE), 
+#                 position = position_dodge(width = 0.9, preserve = "single"),
+#                 width=.2) +
+#   scale_fill_manual(name="Depth/Source",
+#                     values=c("gray75", "gray60", "gray45", "gray30", "dodgerblue3")) +
+#   ylab(expression(Cond.~(mu*S~cm^{-1}))) + xlab("Day of year") +
+#   theme1
+# 
+# turb <- ysi_summ %>% 
+#   filter(var == "turb") %>% 
+#   filter(yday >= 84) %>% 
+#   mutate(depth_range = factor(depth_range, levels = c("[0,1]", "(1,2]", "(2,3]", "(3,4]", "River"), labels = c("0-1m", "1-2m", "2-3m", "3-4m", "River"))) %>% 
+#   mutate(depth_range = replace(depth_range, location == "River", "River")) %>% 
+#   ggplot(aes(x = as.factor(yday), y = conc_mean, fill = depth_range)) +
+#   geom_bar(position = position_dodge(width = 0.9, preserve = "single"), stat = "identity") +
+#   geom_errorbar(aes(ymin = conc_mean-conc_SE, ymax = conc_mean+conc_SE), 
+#                 position = position_dodge(width = 0.9, preserve = "single"),
+#                 width=.2) +
+#   scale_fill_manual(name="Depth/Source",
+#                     values=c("gray75", "gray60", "gray45", "gray30", "dodgerblue3")) +
+#   ylab("Turb. (NTU)") + xlab("Day of year") +
+#   theme1
+# 
+# no3 <- n_summ %>% 
+#   filter(var == "NO3") %>% 
+#   filter(yday >= 84) %>% 
+#   # filter(yday >= 84 & yday <= 115) %>% 
+#   mutate(samp_depth_cat2 = factor(samp_depth_cat2, 
+#                                   levels=c("Top", "Mid-1", "Mid-2", "Mid-3", "Bottom", "River"))) %>% 
+#   ggplot(aes(x = as.factor(yday), y = conc_mean, fill = samp_depth_cat2)) +
+#   geom_bar(position = position_dodge(width = 0.9, preserve = "single"), stat = "identity") +
+#   geom_errorbar(aes(ymin = conc_mean-conc_SE, ymax = conc_mean+conc_SE), 
+#                 position = position_dodge(width = 0.9, preserve = "single"),
+#                 width=.2) +
+#   scale_fill_manual(name="Depth/Source",
+#                     values=c("gray90", "gray75", "gray60", "gray45", "gray30", "dodgerblue3")) +
+#   ylab(expression(paste("NO"["3"]^" -", " (",mu,"mol"," l"^"-1",")"))) + xlab("Day of year") +
+#   theme1
